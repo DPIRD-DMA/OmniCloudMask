8e16776213e8f5fde22a836bc4ce88a892012c98
Nick <wrignj08@gmail.com>
2024-06-13 09:54:08 +0800

    s2 loader clean up

--- omnicloudmask/data_loaders.py
+++ omnicloudmask/data_loaders.py
@@ -9,12 +9,22 @@ from rasterio.profiles import Profile
 
 def load_s2(
     input_path: Union[Path, str],
-    resolution: int = 10,
+    resolution: float = 10.0,
     required_bands: list[str] = ["B04", "B03", "B8A"],
 ) -> tuple[np.ndarray, Profile]:
-    """Load a Sentinel-2 image from a SAFE directory, either L1C or L2A."""
-    if resolution not in [10, 20]:
-        raise ValueError("Resolution must be either 10 or 20")
+    """Load a Sentinel-2 (L1C or L2A) image from a SAFE folder containing the bands"""
+    if not 10 <= resolution <= 50:
+        raise ValueError("Resolution must be between 10 and 50")
+    input_path = Path(input_path)
+    processing_level = find_s2_processing_level(input_path)
+    return open_s2_bands(input_path, processing_level, resolution, required_bands)
+
+
+def find_s2_processing_level(
+    input_path: Path,
+) -> str:
+    """Derive the processing level of a Sentinel-2 image from the folder name."""
+
     folder_name = Path(input_path).name
     processing_level = folder_name.split("_")[1][3:6]
 
@@ -22,115 +32,43 @@ def load_s2(
         raise ValueError(
             f"Processing level {processing_level} not recognized, expected L1C or L2A"
         )
-    if processing_level == "L1C":
-        return load_s2_l1c(input_path, resolution, required_bands)
-    else:
-        return load_s2_l2a(input_path, resolution, required_bands)
+    return processing_level
 
 
-def load_s2_l2a(
-    input_path: Union[Path, str],
-    resolution: int = 10,
-    required_bands: list[str] = ["B04", "B03", "B8A"],
+def open_s2_bands(
+    input_path: Path,
+    processing_level: str,
+    resolution: float,
+    required_bands: list[str],
 ) -> tuple[np.ndarray, Profile]:
-    """Load a Sentinel-2 L2A image from a SAFE directory"""
-    if resolution not in [10, 20]:
-        raise ValueError("Resolution must be either 10 or 20")
-
-    input_path = Path(input_path)
-
-    if resolution == 10:
-        resolution_list = [10, 10, 20]
-    else:
-        resolution_list = [20, 20, 20]
-
-    band_files = {}
-    for band_name, native_resolution in zip(required_bands, resolution_list):
-        # try to get correct res first, if not found, try to get 10m res
-        try:
-            band = list(input_path.rglob(f"*{band_name}_{native_resolution}m.jp2"))[0]
-
-        except IndexError:
-            if resolution == 20:
-                try:
-                    band = list(input_path.rglob(f"*{band_name}_{10}m.jp2"))[0]
-                except IndexError:
-                    raise ValueError(
-                        f"Band {band_name} and resolution of {native_resolution} not found in {input_path} and fallback 10m resolution also not found"
-                    )
-            else:
-                raise ValueError(
-                    f"Band {band_name} and resolution of {native_resolution} not found in {input_path}"
-                )
-        band_files[band_name] = band
-
-    data = []
-    profile = Profile()
+    bands = []
     for band_name in required_bands:
-        with rio.open(band_files[band_name]) as src:
-            if not profile:
-                profile = src.profile
+        if processing_level == "L1C":
+            try:
+                band = list(input_path.rglob(f"*IMG_DATA/*{band_name}.jp2"))[0]
+
+            except IndexError:
+                raise ValueError(f"Band {band_name} not found in {input_path}")
+        else:
+            try:
+                for search_resolution in [10, 20, 60]:
+                    band_paths = list(
+                        input_path.rglob(f"*{band_name}_{search_resolution}m.jp2")
+                    )
+                    if band_paths:
+                        band = band_paths[0]
+                        break
+
+            except IndexError:
+                raise ValueError(f"Band {band_name} not found in {input_path}")
+        with rio.open(band) as src:
+            profile = src.profile
             native_resolution = int(src.res[0])
             scale_factor = native_resolution / resolution
             if native_resolution == resolution:
-                data.append(src.read(1))
+                bands.append(src.read(1))
             else:
-                data.append(
-                    src.read(
-                        1,
-                        out_shape=(
-                            int(src.height * scale_factor),
-                            int(src.width * scale_factor),
-                        ),
-                    )
-                )
-    # update profile with new resolution
-    profile["transform"] = rio.transform.from_origin(  # type: ignore
-        profile["transform"][2],
-        profile["transform"][5],
-        resolution,
-        resolution,
-    )
-    data = np.array(data)
-    # update profile with new width and height
-    profile["height"] = data.shape[1]
-    profile["width"] = data.shape[2]
-
-    return data, profile
-
-
-def load_s2_l1c(
-    input_path: Union[Path, str],
-    resolution: int = 10,
-    required_bands: list[str] = ["B04", "B03", "B8A"],
-) -> tuple[np.ndarray, Profile]:
-    """Load a Sentinel-2 L1C image from a SAFE directory"""
-    if resolution not in [10, 20]:
-        raise ValueError("Resolution must be either 10 or 20")
-
-    input_path = Path(input_path)
-
-    band_files = {}
-    for band_name in required_bands:
-        try:
-            band = list(input_path.rglob(f"*IMG_DATA/*{band_name}.jp2"))[0]
-
-        except IndexError:
-            raise ValueError(f"Band {band_name} not found in {input_path}")
-        band_files[band_name] = band
-
-    data = []
-    profile = Profile()
-    for band_name in required_bands:
-        with rio.open(band_files[band_name]) as src:
-            if not profile:
-                profile = src.profile
-            native_resolution = int(src.res[0])
-            scale_factor = native_resolution / resolution
-            if native_resolution == resolution:
-                data.append(src.read(1))
-            else:
-                data.append(
+                bands.append(
                     src.read(
                         1,
                         out_shape=(
@@ -145,8 +83,7 @@ def load_s2_l1c(
         resolution,
         resolution,
     )
-    data = np.array(data)
-    # update profile with new width and height
+    data = np.array(bands)
     profile["height"] = data.shape[1]
     profile["width"] = data.shape[2]
     return data, profile
